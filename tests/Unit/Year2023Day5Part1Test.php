ce6624393b31e3ab7a8c365c3502625da3bd90e4
Tony Karlsson <tony.karlsson@tjk-systems.fi>
2025-01-07 20:17:48 +0200

    Add unit tests for year 2023 day 5 part 1

---
+++ tests/Unit/Year2023Day5Part1Test.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+use App\AocTasks\Year2023\Day5Part1;
+
+describe('Year2023Day5Part1', function () {
+
+    test('The lowest location number for the example is 35', function () {
+        $task = new Day5Part1();
+        $input = <<<EOL
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+
+        water-to-light map:
+        88 18 7
+        18 25 70
+
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+
+        humidity-to-location map:
+        60 56 37
+        56 93 4
+        EOL;
+        $result = $task->setInput($input)->run()->getResult();
+        expect($result)->toBe('35');
+    });
+
+});
