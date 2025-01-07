5db0dd89c10aba5eb212621787ad9e2d56b82367
Tony Karlsson <tony.karlsson@tjk-systems.fi>
2025-01-07 20:12:19 +0200

    Add unit tests for year 2023 day 4 part 1

---
+++ tests/Unit/Year2023Day4Part1Test.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+use App\AocTasks\Year2023\Day4Part1;
+
+describe('Year2023Day4Part1', function () {
+
+    test('Card 1 is worth 8 points', function () {
+        $task = new Day4Part1();
+        $input = <<<EOL
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        EOL;
+        $result = $task->setInput($input)->run()->getResult();
+        expect($result)->toBe('8');
+    });
+
+    test('Card 2 is worth 2 points', function () {
+        $task = new Day4Part1();
+        $input = <<<EOL
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        EOL;
+        $result = $task->setInput($input)->run()->getResult();
+        expect($result)->toBe('2');
+    });
+
+    test('Card 3 is worth 2 points', function () {
+        $task = new Day4Part1();
+        $input = <<<EOL
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        EOL;
+        $result = $task->setInput($input)->run()->getResult();
+        expect($result)->toBe('2');
+    });
+
+    test('Card 4 is worth 1 point', function () {
+        $task = new Day4Part1();
+        $input = <<<EOL
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        EOL;
+        $result = $task->setInput($input)->run()->getResult();
+        expect($result)->toBe('1');
+    });
+
+    test('Card 5 is worth no points', function () {
+        $task = new Day4Part1();
+        $input = <<<EOL
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        EOL;
+        $result = $task->setInput($input)->run()->getResult();
+        expect($result)->toBe('0');
+    });
+
+    test('Card 6 is worth no points', function () {
+        $task = new Day4Part1();
+        $input = <<<EOL
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        EOL;
+        $result = $task->setInput($input)->run()->getResult();
+        expect($result)->toBe('0');
+    });
+
+    test('Cards 1-6 are worth 13 points in total', function () {
+        $task = new Day4Part1();
+        $input = <<<EOL
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        EOL;
+        $result = $task->setInput($input)->run()->getResult();
+        expect($result)->toBe('13');
+    });
+
+});
