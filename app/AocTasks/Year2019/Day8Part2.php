320c70926366a3dd0df6dfee6e89261b0d41d339
Tony Karlsson <tony.karlsson@tjk-systems.fi>
2025-02-18 20:24:43 +0200

    Implement OCR using tesseract for Day 8 Part 2

--- app/AocTasks/Year2019/Day8Part2.php
+++ app/AocTasks/Year2019/Day8Part2.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\AocTasks\Year2019;
 
 use NorthernBytes\AocHelper\Puzzle;
+use thiagoalessio\TesseractOCR\TesseractOCR;
 
 class Day8Part2 extends Puzzle
 {
@@ -19,17 +20,41 @@ class Day8Part2 extends Puzzle
         $height = 6;
         $layers = array_chunk($inputArray, $width * $height);
 
+        $border = 5;
+        $img = imagecreate($width + 2 * $border, $height + 2 * $border);
+        $white = imagecolorallocate($img, 255, 255, 255);
+        $black = imagecolorallocate($img, 0, 0, 0);
+
         for ($i = 0; $i < $width * $height; $i++) {
             for ($j = 0; $j < count($layers); $j++) {
                 $px_color = $layers[$j][$i];
                 if ($px_color != 2) break;
             }
+
+            // Ascii CLI output
             echo ($px_color == 0) ? '  ' : '██';
             if (($i + 1) % $width == 0) echo "\n";
+
+            // Image output
+            $x = $i % $width;
+            $y = (int)($i / $width);
+            $color = ($px_color == 0) ? $white : $black;
+            imagesetpixel($img, $x + $border, $y + $border, $color);
         }
         echo "\n";
 
-        $this->setPuzzleAnswer('See above ¯\_(ツ)_/¯');
+        $tmpfname = './aoc_d8p2.png';
+        imagepng($img, $tmpfname);
+
+        $ocr = new TesseractOCR();
+        $ocr->image($tmpfname);
+        $ocr->psm(8);
+        $ocr->config('tessedit_char_whitelist', 'ABCDEFGHJKLMNOPQRSTUVWXYZ'); // Assuming no I, avoiding misrecognizing J
+        $answer = $ocr->run();
+
+        unlink($tmpfname);
+
+        $this->setPuzzleAnswer($answer);
 
         return $this;
     }
